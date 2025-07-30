c94d443b7f7adaba35d5d9274de4f480870f7e27
Your Name <you@example.com>
2025-07-30 11:26:03 -0500

    Primera Clase

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print("hola people")
+#Definicion de clases
+class Perro:
+    def __init__(self,nombre,raza):
+        self.nombre=nombre
+        self.raza=raza
+    
+    def Ladrar(self):
+        print(f'{self.nombre} esta ladrando')
+
+class Persona:
+    def __init__(self,nombre,edad,sexo):
+        self.nombre=nombre
+        self.edad=edad
+        self.sexo=sexo
+
+
+#Instanciar Objetos
+
+miPerrito1=Perro("Douglas", "Labrador")
+miPerrito2=Perro("Canela", "Chanda")
+miPerrito3=Perro(input("Ingrese el nombre del perro "),input("Ingrese la raza del perro "))
+
+print (miPerrito1.raza,miPerrito1.nombre)
+print (miPerrito2.raza,miPerrito2.nombre)
+print (miPerrito3.raza,miPerrito3.nombre)
+
+miPerrito3.Ladrar()
