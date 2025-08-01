7b862d6d1df2a1ddff4f64f35dc041983a649aee
Minewarts <mancmancra@gmail.com>
2025-08-01 11:23:39 -0500

    Clase 4

---
+++ calificacionesPOO.py
@@ -0,0 +1,45 @@
+class Estudiante:
+    def __init__(self,nombre,edad):
+        self.nombre=nombre
+        self.edad=edad
+        self.calificaciones=[]
+    
+    def MostrarDatos(self):
+        print(self.nombre,"\n",self.edad,"\n",self.calificaciones)
+    
+    def CalcularPromedio(self):
+        suma=sum(self.calificaciones)
+        promedio=suma/len(self.calificaciones)
+        return promedio
+    
+    def AñadirCalificacion(self,calificacion):
+        self.calificaciones.append(calificacion) 
+
+
+listaEstudiantes=[]
+while True:
+    print("-----//----- Menu -----//-----")
+    print("1.Digitar nuevo estudiante y mostrar su promedio")
+    print("2.Ver todos los registros")
+    print("3.Salir")
+    opcion=int(input("Ingrese su opcion: "))
+    if opcion == 1:
+        nombre=input("Ingrese su nombre: ")
+        edad=int(input("Ingrese su edad: "))
+        estudiante1= Estudiante(nombre,edad)
+
+        for i in range (0,3):
+            calificacion= float(input("Ingrese una nota: "))
+            estudiante1.AñadirCalificacion(calificacion)
+        promedio=estudiante1.CalcularPromedio()
+        print (f'El promedio es: {promedio}')
+        listaEstudiantes.append(estudiante1)
+    
+    elif opcion == 2:
+        numeroEstudiantes=len(listaEstudiantes)
+        print ("el numero de estudiante es: ", numeroEstudiantes)
+            for estudiante  in listaEstudiantes:
+                print
+
+    
+    
